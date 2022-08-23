56f3d20518ada05ad7c1a7031224b54436005300
kevinnevs <kevinnjuguna2@gmail.com>
2022-08-23 23:33:51 +0000

    adding all C files

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+* check_cycle - checks for cycle in a linked list
+* @list: linked list to check
+* Return: 1 if cycle, 0 if not
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t*temp
+
+
