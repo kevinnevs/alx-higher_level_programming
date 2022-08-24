71eeeef9f3c09969b9d74643627fe5298d2f2acd
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-08-25 01:05:40 +0300

    Update 13-main.c

--- 0x01-python-if_else_loops_functions/13-main.c
+++ 0x01-python-if_else_loops_functions/13-main.c
@@ -1,70 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * insert_node - malloc and insert node into sorted singly linked list
- * @head: pointer to head of linked list
- * @number: data for new node
- * Return: address of new node, or NULL if failed
+ * main - check the code for
+ *
+ * Return: Always 0.
  */
-
-listint_t *insert_node(listint_t **head, int number)
+int main(void)
 {
-	listint_t *tmp = NULL;
-	listint_t *new = NULL;
+    listint_t *head;
 
-	if (!head)
-		return (NULL);
+    head = NULL;
+    add_nodeint_end(&head, 0);
+    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 2);
+    add_nodeint_end(&head, 3);
+    add_nodeint_end(&head, 4);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 402);
+    add_nodeint_end(&head, 1024);
+    print_listint(head);
 
-	/* malloc new node */
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = number;
-	new->next = NULL;
+    printf("-----------------\n");
 
-	/* if no linked list, insert node as the only member */
-	if (*head == NULL)
-	{
-		*head = new;
-		(*head)->next = NULL;
-		return (new);
-	}
-	/* if only one node in linked list, do comparision and insert */
-	if ((*head)->next == NULL)
-	{
-		if ((*head)->n < new->n)
-			(*head)->next = new;
-		else
-		{
-			new->next = *head;
-			*head = new;
-		}
-		return (new);
-	}
+    insert_node(&head, 27);
 
-	/* if lots of nodes in linked list, do comparision and insert */
-	tmp = *head;
-	while (tmp->next != NULL)
-	{
-		/* if new node num is smaller than first node, insert */
-		if (new->n < tmp->n)
-		{
-			new->next = tmp;
-			*head = new;
-			return (new);
-		}
-		/* if new node num is the same as an existing node, insert */
-		/* compare previous node and next node, insert in between */
-		if (((new->n > tmp->n) && (new->n < (tmp->next)->n)) ||
-		    (new->n == tmp->n))
-		{
-			new->next = tmp->next;
-			tmp->next = new;
-			return (new);
-		}
-		tmp = tmp->next;
-	}
-	/* if new node is greatest and never inserted, insert now */
-	tmp->next = new;
-	return (new);
+    print_listint(head);
+
+    free_listint(head);
+
+    return (0);
 }
