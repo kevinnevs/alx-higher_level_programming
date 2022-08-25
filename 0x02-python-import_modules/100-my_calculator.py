2eccaa1095379ee7830d6f7b495e8e480d675621
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-08-26 00:43:14 +0300

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     argv = sys.argv[1:]
     argv_count = len(argv)
     operators = ["+", "-", "*", "/"]
-    if argv_count is not 3:
+    if argv_count != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     elif sys.argv[2] not in operators:
