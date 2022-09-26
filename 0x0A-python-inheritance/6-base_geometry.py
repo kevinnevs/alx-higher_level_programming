72e65d2351d27833ad2eba4963ed9dc507ca5ae6
kevinnevs <kevinnjuguna2@gmail.com>
2022-09-26 22:13:39 +0000

    initial commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Public instance area
+    raises Exception message 'area() is not implemented'
+    """
+
+    @property
+    def area(self):
+        """
+        Function not implemented yet
+        """
+        raise Exception("area() is not implemented")
