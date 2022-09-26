b10254baf31c4e8f75f9ec3582b3c81ad5f4f1de
kevinnevs <kevinnjuguna2@gmail.com>
2022-09-26 21:06:23 +0000

    initial commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+lookup - func that returns list of available attr & methods
+of an object
+"""
+
+
+def lookup(obj):
+    """
+    Use dir() function to print list of attr & methods
+    """
+    return dir(obj)
