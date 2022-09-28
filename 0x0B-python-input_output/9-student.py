d0f97cbad9c049dd92664320476d0ea1b5d5d7d2
kevinnevs <kevinnjuguna2@gmail.com>
2022-09-28 19:44:03 +0000

    initial commit

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Student
+"""
+
+
+class Student:
+    """
+    contains student data
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        retrieves dictionary of student
+        """
+
+        return self.__dict__
