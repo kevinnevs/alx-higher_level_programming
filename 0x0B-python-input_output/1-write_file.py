001a894fbff3cf8e55ecf9c29166bd1f3671cbdd
kevinnevs <kevinnjuguna2@gmail.com>
2022-09-28 13:56:46 +0000

    initial commit

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+write_file
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    writes a str to a filename
+    creates file if doesn't exist
+    overwites the content of file if exists
+    """
+
+    with open(filename, mode="w", encoding="utf-8") as writeFile:
+        if len(text) > 0:
+            writeFile.write(text)
+        else:
+            writeFile.write(text)
+    return text
