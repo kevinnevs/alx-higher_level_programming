5579825c0918ed74cc4fc8613e00aefca84c290a
kevinnevs <kevinnjuguna2@gmail.com>
2022-11-29 09:16:33 +0000

    initial commit, fixing semistandard

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*
+Script that prints 3 lines: (like 1-muli_languages.js) by using array and loop
+The first line is: "C is fun"
+The second line is: "Python is cool"
+The third line is: "JavaScript is amazing"
+*/
+
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
