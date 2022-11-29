f174dcb72a4c1cc87f995943b570152a191cfef3
kevinnevs <kevinnjuguna2@gmail.com>
2022-11-29 09:52:19 +0000

    initial commit

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/*
+script that prints the addition of 2 integers
+*/
+
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
+const res = num1 + num2;
+
+console.log(res);
