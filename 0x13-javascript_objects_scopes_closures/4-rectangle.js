3e75762a6a945f73d416cc2c60022bc5dc75c5a7
kevinnevs <kevinnjuguna2@gmail.com>
2022-11-30 14:02:26 +0000

    initial commit

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+/*
+Class rectangle that defines a rectangle
+*/
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    this.width = [this.height, this.width = this.height][0];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
