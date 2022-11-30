b22b324fda18d84c598731f1f25c3e002bef09e0
kevinnevs <kevinnjuguna2@gmail.com>
2022-11-30 18:56:12 +0000

    initial commit

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/*
+function that prints no of arguments already printed & the new arg
+*/
+let n = 0;
+exports.logMe = function (item) {
+  console.log(n + ':' + item);
+  n++;
+};
