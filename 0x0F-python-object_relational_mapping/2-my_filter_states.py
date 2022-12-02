369d97699511f40fc5b2b2b6ee9b31d10965b8a4
kevinnevs <kevinnjuguna2@gmail.com>
2022-12-02 12:16:52 -0800

    initial commit

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Takes an argument and displays values of State, where name matches argument
+"""
+
+import MySQLdb
+import sys
+
+
+def filter_names():
+    """
+    Takes arguments argv to list databases
+
+    Arguments:
+        argv[1]: mysql username
+        argv[2]: mysql password
+        argv[3]: database name
+        argv[4]: argument name
+    """
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE BINARY name='{:s}'\
+                ORDER BY id ASC".format(sys.argv[4]))
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    filter_names()
