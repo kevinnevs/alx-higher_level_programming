fd9822e18fe170372c210bed5d7580fe88cfa97d
kevinnevs <kevinnjuguna2@gmail.com>
2022-12-02 12:42:45 -0800

    fixing code

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -18,19 +18,20 @@ def filter_names_safe():
         argv[3]: database name
         argv[4]: argument name
     """
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3])
+    if len(sys.argv) == 5:
+        db = MySQLdb.connect(host="localhost",
+                             port=3306,
+                             user=sys.argv[1],
+                             passwd=sys.argv[2],
+                             db=sys.argv[3])
 
-    cur = db.cursor()
+        cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE BINARY name='{:s}'\
-                ORDER BY id ASC".format(sys.argv[4]))
-    rows = cur.fetchall()
-    for i in rows:
-        print(i)
+        cur.execute("SELECT * FROM states WHERE BINARY name='{:s}'\
+                    ORDER BY id ASC".format(sys.argv[4]))
+        rows = cur.fetchall()
+        for i in rows:
+            print(i)
 
         cur.close()
         db.close()
