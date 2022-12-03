33459d1aca0159e0318547c1feb52cbda4329282
kevinnevs <kevinnjuguna2@gmail.com>
2022-12-03 13:18:39 -0800

    initial commit

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Script that prints the first state objects from db hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def list_state_obj():
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    """create a session"""
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    """query all State objects from the database"""
+    state = session.query(State).first()
+
+    """print each State object"""
+    if state is not None:
+        print("{}: {}".format(state.id, state.name))
+
+    else:
+        print("Nothing")
+
+    """closes session"""
+    session.close()
+
+
+if __name__ == "__main__":
+    list_state_obj()
