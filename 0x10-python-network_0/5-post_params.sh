5eab681ed2163aa017d7d8c963833dc813d15783
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-12-16 17:31:27 +0300

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes URL, sends a POST request to passed URL, and displays body response, with specified email and subject
-curl -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -sX POST -sd "email=test@gmail.com&subject=I will always be here for PLD" "$1"
