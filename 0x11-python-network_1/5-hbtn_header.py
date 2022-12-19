a1c09b48d1c055a5ae33f76610e6c1ead405f7fb
kevinnevs <kevinnjuguna2@gmail.com>
2022-12-19 03:42:12 -0800

    initial commit

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,8 +6,13 @@ takes URL, sends request to URL, and displays the value of the variable
 import requests
 import sys
 
-if __name__ == "__main__":
+
+def request_header():
     url = sys.argv[1]
     response = requests.get(url)
     request_id = response.headers['X-Request-Id']
     print(request_id)
+
+
+if __name__ == "__main__":
+    request_header()
