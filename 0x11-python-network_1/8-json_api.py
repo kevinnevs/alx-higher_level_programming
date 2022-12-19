37570557b7cc063c08f6fed4d764e25521a1341f
kevinnevs <kevinnjuguna2@gmail.com>
2022-12-19 01:55:04 -0800

    initial commit

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+takes in a letter and sends a POST requests to
+'http://0.0.0.0:5000/search_user' with the letter as a parameter
+"""
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        letter = sys.argv[1]
+    else:
+        letter = ""
+
+    response = requests.post('http://0.0.0.0:5000/search_user',
+                             data={'q': letter})
+
+    try:
+        data = response.json()
+    except ValueError:
+        print("Not a valid JSON")
+        sys.exit()
+
+    if not data:
+        print("No reesult")
+    else:
+        print("[{}] {}".format(data['id'], data['name']))
