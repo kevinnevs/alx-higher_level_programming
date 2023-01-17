d447d51ac066c409594276bb2205b47a2d8296af
kevinnevs <kevinnjuguna2@gmail.com>
2023-01-17 20:04:54 +0000

    initial commit

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/*
+script that gets contents of a webpage and stores in a file
+*/
+const url = process.argv[2];
+const filePath = process.argv[3];
+const fs = require('fs');
+const request = require('request');
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFile(filePath, body, 'utf8');
+  }
+});
