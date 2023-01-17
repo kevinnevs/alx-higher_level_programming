c9f426c57105205a4c50a9c1a68bb73f3b47f5ed
kevinnevs <kevinnjuguna2@gmail.com>
2023-01-17 18:46:29 +0000

    initial commit

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*
+script that writes a string to a file
+*/
+const fs = require('fs');
+const filePath = process.argv[2];
+const data = process.argv[3];
+
+fs.writeFile(filePath, data, 'utf8', (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
