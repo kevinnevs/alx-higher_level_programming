0d0cfa4d73b2b7edd3d55c3eb9802b6d69680485
kevinnevs <kevinnjuguna2@gmail.com>
2023-01-17 18:32:18 +0000

    fixing semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,13 +2,13 @@
 /*
 script that reads and prints content of a file
 */
-const fs = require('fs')
-const filePath = process.argv[2]
+const fs = require('fs');
+const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf8', (err, data) => {
    if (err) {
-     console.log(err)
+     console.log(err);
    } else {
-     console.log(data)
+     console.log(data);
    }
-})
+});
