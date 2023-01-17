7af20a82b0a2a5eee25c0fc68071acfb987ba08c
kevinnevs <kevinnjuguna2@gmail.com>
2023-01-17 18:29:26 +0000

    initial commit

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*
+script that reads and prints content of a file
+*/
+const fs = require('fs')
+const filePath = process.argv[2]
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+   if (err) {
+     console.log(err)
+   } else {
+     console.log(data)
+   }
+})
